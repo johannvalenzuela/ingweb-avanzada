d4b68f8dabc38d29669ad4db46d54428ea773e64
Johann <johann.aevt@gmail.com>
2017-08-27 01:23:13 -0300

    templates

---
+++ navegadorcito/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import *
+from django.contrib import admin
+from navegadorcito.views import index, login_admin, login_student, login_teacher, courses_teacher
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^student/', login_student),
+    url(r'^teacher/', login_teacher),
+    url(r'^teacher/courses', courses_teacher),
+    url(r'^admin/', login_admin),
+]
